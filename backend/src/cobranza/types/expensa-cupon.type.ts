15b3b2991497e1ce0a042c1fa4a10b92e12e9111
Lucas <lucas.godoy@ucc.edu.ar>
2026-08-21 17:52:30 +0000

    add: creamos type expensa cupon

---
+++ backend/src/cobranza/types/expensa-cupon.type.ts
@@ -0,0 +1,16 @@
+export interface Vencimiento {
+  fecha: string | null;
+  monto: number | null;
+}
+
+export interface ExpensaCupon {
+  edificio: string | null;
+  ubicacion: string | null;
+  titular: string | null;
+  periodo: string | null;
+  expensas_ordinarias_total: number | null;
+  porcentaje_participacion: number | null;
+  expensas_ordinarias_monto: number | null;
+  vencimiento_1: Vencimiento | null;
+  vencimiento_2: Vencimiento | null;
+}
